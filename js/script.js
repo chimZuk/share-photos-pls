d3aba33990e3d378f78fadf16793c6e4c034a791
Dzmitry Kuzmitch <kuzya19989@outlook.com>
2019-12-29 01:18:26 -0500

    Added next-prev functionality

--- js/script.js
+++ js/script.js
@@ -5,13 +5,33 @@ var nav1 = $("#nav1");
 var nav2 = $("#nav2");
 
 var data = {};
+var current_range = "1_10";
 
 $.ajax({
     url: "/library/washington_dc/map.json",
     success: function (map) {
         data = map;
         renred_nav(data);
-        render(1, 10);
+
+        var value = localStorage.getItem("range");
+        console.log(value);
+        if (value) {
+            var range = value.split("_");
+            var start = Number(range[0]);
+            var end = Number(range[1]);
+            if (end <= Number(data.number)) {
+                $("#nav1_selector").val(value);
+                $("#nav2_selector").val(value);
+                current_range = value;
+                render(start, end);
+            } else {
+                current_range = "1_10";
+                render(1, 10);
+            }
+        } else {
+            current_range = "1_10";
+            render(1, 10);
+        }
     }
 });
 
@@ -20,14 +40,19 @@ function renred_nav(data = data) {
     var number = Math.floor(amount / 10) + 1;
 
     var nav = ``;
-    var nav1_el = `<select id="nav1_selector" onchange="pre_render(this);">`;
-    var nav2_el = `<select id="nav2_selector" onchange="pre_render(this);">`;
+    var nav1_el = `
+        <a onclick="pre_render_next(-1)" class="next-link">Previous</a>
+        <select id="nav1_selector" onchange="pre_render(this);">`;
+    var nav2_el = `
+        <a onclick="pre_render_next(-1)" class="next-link">Previous</a>
+        <select id="nav2_selector" onchange="pre_render(this);">`;
     for (var i = 1; i <= number; i++) {
         var start = (Number((i - 1) * 10) + Number(1));
         var end = (i * 10 <= data.number) ? i * 10 : data.number;
         nav += `<option value="` + start + `_` + end + `">` + start + `-` + end + `</a>`
     }
-    nav += `</select>`;
+    nav += `</select>
+        <a onclick="pre_render_next(1)" class="next-link">Next</a>`;
 
     nav1_el += nav;
     nav2_el += nav;
@@ -45,7 +70,34 @@ function pre_render(element) {
     $("#nav1_selector").val(value);
     $("#nav2_selector").val(value);
 
+    current_range = value;
+    localStorage.setItem("range", current_range);
+
     render(start, end);
+}
+
+function pre_render_next(sign) {
+    var value = current_range;
+    var range = value.split("_");
+    var start = Number(range[0]) + (10 * sign);
+    var end = Number(range[0]) + 10 + (10 * sign) - 1;
+
+    if (start > 0 && end < Number(data.number)) {
+        $("#nav1_selector").val(start + "_" + end);
+        $("#nav2_selector").val(start + "_" + end);
+        current_range = start + "_" + end;
+        localStorage.setItem("range", current_range);
+        render(start, end);
+    } else {
+        if (start > 0 && start <= Number(data.number)) {
+            end = Number(data.number);
+            $("#nav1_selector").val(start + "_" + end);
+            $("#nav2_selector").val(start + "_" + end);
+            current_range = start + "_" + end;
+            localStorage.setItem("range", current_range);
+            render(start, end);
+        }
+    }
 }
 
 function render(start, end) {
@@ -58,7 +110,7 @@ function render(start, end) {
         photos.append(
             `<div class="image-post">
                 <img src="/library/washington_dc/shrinked/` + name + `.jpg">
-                <h2 style="margin: 5px;">` + name + `.jpg</h2>
+                <h4 style="margin: 5px;">` + name + `.jpg</h4>
                 <div class="action-buttons">
                     <button onclick="download_iphone('/library/washington_dc/reg/` + name + `.jpg', '` + name + `.jpg')">Download for iPhone 🍎💩</button>
                     <button onclick="download('/library/washington_dc/reg/` + name + `.jpg', '` + name + `.jpg')">Regular Download 🤑</button>
